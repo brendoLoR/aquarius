9a9e24b3678a6f3b760c34cda4ea35df387113e8
brendo <brendoja@gmail.com>
2021-05-27 22:57:51 -0300

    gitignore test

--- app/config/aquarius.php
+++ app/config/aquarius.php
@@ -1,10 +1,10 @@
 <?php
 return [
-    'host'  =>  "localhost",
-    'port'  =>  "5432",
-    'name'  =>  "aquarius",
-    'user'  =>  "system",
-    'pass'  =>  "sustem",
-    'type'  =>  "pgsql",
-    'prep'  =>  "1"
+    'host'   => "localhost",
+    'port'   => "5432",
+    'name'   => "aquariusDev",
+    'user'   => "system",
+    'pass'   => "system",
+    'type'   => "pgsql",
+    'prep'   => "1"
 ];
